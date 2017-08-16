c9ae4b7532b6c6587e8e2281193af09db27c99f1
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-08-15 22:08:47 -0700

    setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () =>
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>Welcome to your new Gatsby site hosted on Netlfy. To enter for a chance to win the Nintendo Switch</p>
+    <p>Here is some info on Netlify and here is a <a href="#">link to the documentation</a></p>
+    <a href="#">Finish submission</a> then go to 
   </div>
 
 export default IndexPage
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,7 +3,6 @@ import Link from 'gatsby-link'
 
 const SecondPage = () =>
   <div>
-    <h1>Hi people</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
