3beee4418886aca19c1a76c5154fccfb0d3c63a1
Brian Douglas <bdougie@users.noreply.github.com>
2018-02-07 15:17:51 -0800

    updated repo link

--- src/pages/step-1.js
+++ src/pages/step-1.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const SecondPage = () =>
   <div>
     <p>To get started click the button below.</p>
-    <a href="https://app.netlify.com/start/deploy?repository=https://github.com/netlify/nodevember-gatsby-app">
+    <a href="https://app.netlify.com/start/deploy?repository=https://github.com/netlify/">
       <img src="https://www.netlify.com/img/deploy/button.svg" title="Deploy to Netlify" />
     </a>
     <p>
@@ -15,13 +15,13 @@ const SecondPage = () =>
       React applications. To enter 
       To find out how you can leverage some of Gatsby's built in
       features, like Prerendering, Scalable Deploys, and <a
-        href="https://www.netlify.com/blog/2017/07/18/http/2-server-push-on-netlify/">HTTP/2
+        href="https://www.netlify.com/blog/2017/07/18/http/2-server-push-on-netlify/deploy-from-table-app">HTTP/2
         Push</a>, check out the Netlify <a
         href="https://www.netlify.com/docs/">documentation</a>. You can learn more about <a href="https://www.gatsbyjs.org/">Gatsby</a>,
       a static site generator build with React and GraphQL. 
       <br />
       <br />
-      If you want to view the source code, you can do so at <a href="https://github.com/netlify/nodevember-gatsby-app">netlify/nodevember-gatsby-app
+      If you want to view the source code, you can do so at <a href="https://github.com/netlify/deploy-from-table-app">netlify/nodevember-gatsby-app
       </a><br />
     </small>
   </div>
